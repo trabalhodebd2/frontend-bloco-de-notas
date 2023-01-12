e3f5bd1c420e0f22609412d2f6953e8f5d8fa21b
Pedro Júnior <pedrojuniorifpb@gmail.com>
2023-01-12 00:29:57 -0300

    Terminando de estilizar os modais

--- js/tailwind/tailwind.config.js
+++ js/tailwind/tailwind.config.js
@@ -4,6 +4,7 @@ tailwind.config = {
             colors: {
                 primary: '#F5F5F5',
                 secondary: '#00000080',
+                tertiary: '#D90429',
             },
         }
     }
